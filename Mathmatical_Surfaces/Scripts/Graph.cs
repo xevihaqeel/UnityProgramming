51e25908846d1ad926b25373e1d2c96b894ecfcc
Justin B. Kinard <thekinardist@hotmail.com>
2023-07-23 11:42:38 -0400

    Updating Math Surfaces

--- Mathmatical_Surfaces/Scripts/Graph.cs
+++ Mathmatical_Surfaces/Scripts/Graph.cs
@@ -1,37 +1,48 @@
 using UnityEngine;
+using static UnityEngine.Mathf;
 
 public class Graph : MonoBehaviour {
     [SerializeField]
     Transform pointPrefab;
     //create an array of Transforms for later use   
     Transform[] points;
+    float duration;
+
+    bool transitioning;
+
+    FunctionLibrary.FunctionName transitionFunction;
 
 
-    [SerializeField, Range(10,100)]
+    [SerializeField, Range(10,200)]
     int resolution = 10;
 
     //creates field that allows for the function to be changed
-    [SerializeField, Range(0,2)]
-    int function;
+
+	[SerializeField]
+	FunctionLibrary.FunctionName function;
+
+    public enum TransitionMode {Cycle, Random}
+
+    [SerializeField]
+    TransitionMode transitionMode;
+
+    [SerializeField, Min(0f)]
+    float functionDuration = 1f, transitionDuration = 1f;
+
 
     void Awake(){
 
         //keeps the size of the cubes within the 
         float step = 2f/resolution;
-       	var position = Vector3.zero * step;
+       	// var position = Vector3.zero * step;
 		var scale = Vector3.one * step;
         
-        //sets the array value to the the designated float
-        points = new Transform[resolution];
-
+        points = new Transform[resolution * resolution];
+        //this loop explicitly keeps track fo the x and y coordinates
 		for (int i = 0; i < points.Length; i++) {
+
             //assign the instantiated points to be the points designated by loop/array
 			Transform point = points[i] = Instantiate(pointPrefab);
-            // points[i] = point; 
-			position.x = (i + 0.5f) * step - 1f;
-
-            // position.y = Mathf.Pow(position.x,3f);
-			point.localPosition = position;
 			point.localScale = scale;
             //ensures that created points remain children of the object holding the script creating them
             point.SetParent(transform,false);
@@ -39,21 +50,79 @@ public class Graph : MonoBehaviour {
     }
 
     void Update(){
-        //run through the point array and set position for each point
-        float time = Time.time;
-        for(int i = 0; i < points.Length; i++){
-            Transform point = points[i];
-            Vector3 position = point.localPosition;
-            if(function == 0){
-            position.y = FunctionLibrary.Wave(position.x, time);
+        duration += Time.deltaTime;
+        if(transitioning){
+            if(duration >= transitionDuration){
+                duration -= transitionDuration;
+                transitioning = false;
             }
-            else if(function == 1){
-                position.y = FunctionLibrary.MultiWave(position.x,time);
-            }
-            else{
-                position.y = FunctionLibrary.Ripple(position.x, time); 
-            }
-            point.localPosition = position;
+        }
+        else if (duration >= functionDuration){
+            duration -= functionDuration;
+            transitioning = true;
+            transitionFunction = function;
+            PickNextFunction();
+        }
+        if(transitioning){
+            UpdateFunctionTransition();
+        }
+        else{
+            UpdateFunction();
         }
     }
+
+
+    void PickNextFunction(){
+        function = transitionMode == TransitionMode.Cycle ?
+        FunctionLibrary.GetNextFunctionName(function) : 
+        FunctionLibrary.GetRandomFunctionNameOtherThan(function);
+    }
+    void UpdateFunction(){
+        
+		FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
+    
+        float time = Time.time;
+        float step = 2f/resolution;
+
+        //run through the point array and set position for each point
+        float v = 0.5f * step - 1f;
+        for(int i = 0, x = 0, z = 0; i < points.Length; i++, x++){
+            //checks if coordinate is equal to resolution; if so, it resets it to zero 
+            if(x == resolution){
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = f(u, v, time);
+        }
+
+    }
+
+      void UpdateFunctionTransition(){
+        
+		FunctionLibrary.Function from = FunctionLibrary.GetFunction(transitionFunction),
+            to = FunctionLibrary.GetFunction(function);
+        float progress = duration / transitionDuration;
+        float time = Time.time;
+        float step = 2f/resolution;
+
+        //run through the point array and set position for each point
+        float v = 0.5f * step - 1f;
+        for(int i = 0, x = 0, z = 0; i < points.Length; i++, x++){
+            //checks if coordinate is equal to resolution; if so, it resets it to zero 
+            if(x == resolution){
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = FunctionLibrary.Morph(
+                u, v, time, from, to, progress
+            );
+        }
+
+    }
+
+ 
 }   
